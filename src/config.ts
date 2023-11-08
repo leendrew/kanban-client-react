6c63181ad154ca780e6c3ba11c3fffc35d014081
leendrew <leendrew99@gmail.com>
2023-11-08 21:43:37 +0600

    feat: config

---
+++ src/config.ts
@@ -0,0 +1,37 @@
+type EnvType = Record<string, { key: string; type: string | number | boolean }>;
+
+const env = {
+  baseUrl: { key: 'BASE_URL', type: String() },
+  port: { key: 'VITE_PORT', type: Number() },
+  mode: { key: 'MODE', type: String() },
+  isDev: { key: 'DEV', type: Boolean() },
+  isProd: { key: 'PROD', type: Boolean() },
+} satisfies EnvType;
+
+type ConfigType<T extends EnvType> = { [Key in keyof T]: T[Key]['type'] };
+
+export type Config = ConfigType<typeof env>;
+
+export const config = Object.entries(env).reduce((acc, [key, value]) => {
+  // @ts-expect-error just because
+  acc[key] = checkEnv(value.key, value.type);
+  return acc;
+}, {} as Config);
+
+function checkEnv(env: string, type: string | number | boolean): string | number | boolean {
+  const envValue = import.meta.env[env];
+  if (envValue === undefined) {
+    throw new Error(`Check that '${env}' really exist in your .env file`);
+  }
+
+  switch (typeof type) {
+    case 'string':
+      return envValue;
+    case 'number':
+      return parseInt(envValue, 10);
+    case 'boolean':
+      return !!envValue;
+    default:
+      throw new Error('Wrong env type, expected string | number | boolean');
+  }
+}
