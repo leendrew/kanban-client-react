0cd146f6fbf2914829a96e3007a20b44c49d4af8
leendrew <leendrew99@gmail.com>
2023-11-12 00:07:08 +0600

    feat: logo

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { resolve, join } from 'node:path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   return defineConfig({
     base: './',
-    plugins: [react()],
+    plugins: [react(), svgr()],
     resolve: {
       alias: {
         '@': resolve(__dirname, join('.', 'src')),
