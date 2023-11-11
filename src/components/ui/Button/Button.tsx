fa57cad40ecb1cf6e71c7f322eaddc00df1b03d1
leendrew <leendrew99@gmail.com>
2023-11-11 11:56:52 +0600

    feat: loading button

---
+++ src/components/ui/Button/Button.tsx
@@ -0,0 +1,29 @@
+import { Button as MuiButton, Stack, CircularProgress } from '@mui/material';
+import type { ButtonProps } from '@mui/material';
+
+interface LoadingButtonProps extends ButtonProps {
+  loading?: boolean;
+  loadingText?: string;
+}
+
+export function Button({
+  loading = false,
+  loadingText = 'Pending',
+  children,
+  disabled,
+  ...rest
+}: LoadingButtonProps) {
+  return (
+    <MuiButton disabled={disabled || loading} {...rest}>
+      <Stack direction="row" alignItems="center" gap={1}>
+        {loading && (
+          <>
+            <CircularProgress color="inherit" size="1rem" />
+            {loadingText}
+          </>
+        )}
+        {!loading && children}
+      </Stack>
+    </MuiButton>
+  );
+}
