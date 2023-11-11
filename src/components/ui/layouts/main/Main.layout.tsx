9e624494a8ab3951833ecb676b45ace97e710d13
leendrew <leendrew99@gmail.com>
2023-11-12 00:10:00 +0600

    chore: layouts

--- src/components/ui/layouts/main/Main.layout.tsx
+++ src/components/ui/layouts/main/Main.layout.tsx
@@ -1,9 +1,28 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
+import { PATHS } from '@/router';
+import { Box, Container, Stack } from '@mui/material';
+import Logo from '@/assets/logo.svg?react';
 
 export function MainLayout() {
   return (
     <>
-      <header className="header">Header</header>
+      <Box
+        component="header"
+        sx={{
+          minHeight: '4rem',
+          borderBottom: '1px solid var(--border-color_main)',
+        }}
+      >
+        <Container sx={{ height: '100%' }}>
+          <Stack sx={{ height: '100%' }} direction="row" alignItems="center">
+            <Link to={PATHS.home}>
+              <Box sx={{ width: 130, height: 32 }}>
+                <Logo style={{ width: '100%', height: '100%' }} />
+              </Box>
+            </Link>
+          </Stack>
+        </Container>
+      </Box>
       <main className="main">
         <Outlet />
       </main>
