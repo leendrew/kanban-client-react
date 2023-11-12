7b0a7f6ebfeac8640828a1f8374d3fb2c0962d3a
leendrew <leendrew99@gmail.com>
2023-11-12 16:37:31 +0600

    feat: Sheet base component

--- src/pages/auth/login/Login.tsx
+++ src/pages/auth/login/Login.tsx
@@ -3,7 +3,7 @@ import { Container, Box, Stack, Typography, TextField } from '@mui/material';
 import { Controller, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { PATHS } from '@/config';
-import { Button } from '@/components/ui';
+import { Sheet, Button } from '@/components/ui';
 import { loginSchema } from '@/schemas';
 import Logo from '@/assets/logo.svg?react';
 import { useLoginMutation } from '@/store/auth';
@@ -38,66 +38,73 @@ export function Login() {
 
   return (
     <>
-      <Container sx={{ height: '100%' }}>
-        <Stack
+      <Container>
+        <Box
           sx={{
-            maxWidth: '20rem',
-            padding: '2rem 1.5rem',
-            margin: '2rem auto',
-            borderRadius: '0.5rem',
-            backgroundColor: 'var(--bg-color_main)',
-            boxShadow: 'var(--box-shadow_main)',
+            padding: '2rem 0 0',
           }}
-          flexDirection="column"
-          alignItems="center"
-          gap={2}
         >
-          <Stack component="form" sx={{ width: '100%' }} gap={2} onSubmit={handleSubmit(onSubmit)}>
-            <Stack direction="column" alignItems="center">
-              <Box sx={{ width: 130, height: 32 }}>
-                <Logo style={{ width: '100%', height: '100%' }} />
+          <Sheet
+            sx={{
+              maxWidth: '20rem',
+              margin: '0 auto',
+              padding: '2rem 1.5rem',
+            }}
+          >
+            <Stack flexDirection="column" alignItems="center" gap={2}>
+              <Stack
+                component="form"
+                sx={{ width: '100%' }}
+                gap={2}
+                onSubmit={handleSubmit(onSubmit)}
+              >
+                <Stack direction="column" alignItems="center">
+                  <Box sx={{ width: 130, height: 32 }}>
+                    <Logo style={{ width: '100%', height: '100%' }} />
+                  </Box>
+                  <Typography component="h1" variant="h5" sx={{ margin: '1rem 0' }}>
+                    Login
+                  </Typography>
+                </Stack>
+                <Controller
+                  name="login"
+                  control={control}
+                  render={({ field }) => (
+                    <TextField
+                      label="Login"
+                      variant="outlined"
+                      size="small"
+                      error={!!errors.login}
+                      helperText={errors?.login?.message as string}
+                      {...field}
+                    />
+                  )}
+                />
+                <Controller
+                  name="password"
+                  control={control}
+                  render={({ field }) => (
+                    <TextField
+                      label="Password"
+                      variant="outlined"
+                      size="small"
+                      error={!!errors.password}
+                      helperText={errors?.password?.message as string}
+                      {...field}
+                    />
+                  )}
+                />
+                <Button type="submit" variant="contained" loading={isLoading}>
+                  Login
+                </Button>
+              </Stack>
+              <Box>
+                Didn't have account?&nbsp;
+                <Link to={PATHS.auth.register}>Register</Link>
               </Box>
-              <Typography component="h1" variant="h5" sx={{ margin: '1rem 0' }}>
-                Login
-              </Typography>
             </Stack>
-            <Controller
-              name="login"
-              control={control}
-              render={({ field }) => (
-                <TextField
-                  label="Login"
-                  variant="outlined"
-                  size="small"
-                  error={!!errors.login}
-                  helperText={errors?.login?.message as string}
-                  {...field}
-                />
-              )}
-            />
-            <Controller
-              name="password"
-              control={control}
-              render={({ field }) => (
-                <TextField
-                  label="Password"
-                  variant="outlined"
-                  size="small"
-                  error={!!errors.password}
-                  helperText={errors?.password?.message as string}
-                  {...field}
-                />
-              )}
-            />
-            <Button type="submit" variant="contained" loading={isLoading}>
-              Login
-            </Button>
-          </Stack>
-          <Box>
-            Didn't have account?&nbsp;
-            <Link to={PATHS.auth.register}>Register</Link>
-          </Box>
-        </Stack>
+          </Sheet>
+        </Box>
       </Container>
     </>
   );
--- src/pages/auth/register/Register.tsx
+++ src/pages/auth/register/Register.tsx
@@ -3,7 +3,7 @@ import { Container, Box, Stack, Typography, TextField } from '@mui/material';
 import { Controller, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { PATHS } from '@/config';
-import { Button } from '@/components/ui';
+import { Sheet, Button } from '@/components/ui';
 import { registerSchema } from '@/schemas';
 import Logo from '@/assets/logo.svg?react';
 import { useRegisterMutation } from '@/store/auth';
@@ -40,94 +40,101 @@ export function Register() {
 
   return (
     <>
-      <Container sx={{ height: '100%' }}>
-        <Stack
+      <Container>
+        <Box
           sx={{
-            maxWidth: '20rem',
-            padding: '2rem 1.5rem',
-            margin: '2rem auto',
-            borderRadius: '0.5rem',
-            backgroundColor: 'var(--bg-color_main)',
-            boxShadow: 'var(--box-shadow_main)',
+            padding: '2rem 0 0',
           }}
-          flexDirection="column"
-          alignItems="center"
-          gap={2}
         >
-          <Stack component="form" sx={{ width: '100%' }} gap={2} onSubmit={handleSubmit(onSubmit)}>
-            <Stack direction="column" alignItems="center">
-              <Box sx={{ width: 130, height: 32 }}>
-                <Logo style={{ width: '100%', height: '100%' }} />
+          <Sheet
+            sx={{
+              maxWidth: '20rem',
+              margin: '0 auto',
+              padding: '2rem 1.5rem',
+            }}
+          >
+            <Stack flexDirection="column" alignItems="center" gap={2}>
+              <Stack
+                component="form"
+                sx={{ width: '100%' }}
+                gap={2}
+                onSubmit={handleSubmit(onSubmit)}
+              >
+                <Stack direction="column" alignItems="center">
+                  <Box sx={{ width: 130, height: 32 }}>
+                    <Logo style={{ width: '100%', height: '100%' }} />
+                  </Box>
+                  <Typography component="h1" variant="h5" sx={{ margin: '1rem 0' }}>
+                    Register
+                  </Typography>
+                </Stack>
+                <Controller
+                  name="name"
+                  control={control}
+                  render={({ field }) => (
+                    <TextField
+                      label="Name"
+                      variant="outlined"
+                      size="small"
+                      error={!!errors.name}
+                      helperText={errors?.name?.message as string}
+                      {...field}
+                    />
+                  )}
+                />
+                <Controller
+                  name="login"
+                  control={control}
+                  render={({ field }) => (
+                    <TextField
+                      label="Login"
+                      variant="outlined"
+                      size="small"
+                      error={!!errors.login}
+                      helperText={errors?.login?.message as string}
+                      {...field}
+                    />
+                  )}
+                />
+                <Controller
+                  name="password"
+                  control={control}
+                  render={({ field }) => (
+                    <TextField
+                      label="Password"
+                      variant="outlined"
+                      size="small"
+                      error={!!errors.password}
+                      helperText={errors?.password?.message as string}
+                      {...field}
+                    />
+                  )}
+                />
+                <Controller
+                  name="confirmPassword"
+                  control={control}
+                  render={({ field }) => (
+                    <TextField
+                      label="Confirm Password"
+                      variant="outlined"
+                      size="small"
+                      error={!!errors.confirmPassword}
+                      helperText={errors?.confirmPassword?.message as string}
+                      {...field}
+                    />
+                  )}
+                />
+                <Button type="submit" variant="contained" loading={isLoading}>
+                  Register
+                </Button>
+              </Stack>
+              <Box>
+                Already have account?&nbsp;
+                <Link to={PATHS.auth.login}>Login</Link>
               </Box>
-              <Typography component="h1" variant="h5" sx={{ margin: '1rem 0' }}>
-                Register
-              </Typography>
             </Stack>
-            <Controller
-              name="name"
-              control={control}
-              render={({ field }) => (
-                <TextField
-                  label="Name"
-                  variant="outlined"
-                  size="small"
-                  error={!!errors.name}
-                  helperText={errors?.name?.message as string}
-                  {...field}
-                />
-              )}
-            />
-            <Controller
-              name="login"
-              control={control}
-              render={({ field }) => (
-                <TextField
-                  label="Login"
-                  variant="outlined"
-                  size="small"
-                  error={!!errors.login}
-                  helperText={errors?.login?.message as string}
-                  {...field}
-                />
-              )}
-            />
-            <Controller
-              name="password"
-              control={control}
-              render={({ field }) => (
-                <TextField
-                  label="Password"
-                  variant="outlined"
-                  size="small"
-                  error={!!errors.password}
-                  helperText={errors?.password?.message as string}
-                  {...field}
-                />
-              )}
-            />
-            <Controller
-              name="confirmPassword"
-              control={control}
-              render={({ field }) => (
-                <TextField
-                  label="Confirm Password"
-                  variant="outlined"
-                  size="small"
-                  error={!!errors.confirmPassword}
-                  helperText={errors?.confirmPassword?.message as string}
-                  {...field}
-                />
-              )}
-            />
-            <Button type="submit" variant="contained" loading={isLoading}>
-              Register
-            </Button>
-          </Stack>
-          <Box>
-            Already have account?&nbsp;
-            <Link to={PATHS.auth.login}>Login</Link>
-          </Box>
-        </Stack>
+          </Sheet>
+        </Box>
       </Container>
     </>
   );
