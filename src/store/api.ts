42139ce576fb7cfd61e68e8a36152f0795355c99
leendrew <leendrew99@gmail.com>
2023-11-13 00:25:05 +0600

    feat: reauth query

--- src/store/api.ts
+++ src/store/api.ts
@@ -1,28 +1,68 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { config } from '@/config';
-import { reducerKey } from './constants';
+import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
+import type { BaseQueryFn, FetchArgs, FetchBaseQueryError } from '@reduxjs/toolkit/query/react';
 import type { RootState } from './store';
+import { HTTP } from './constants';
+import { config, PATHS } from '@/config';
 
 const baseQuery = fetchBaseQuery({
   baseUrl: config.apiUrl,
   prepareHeaders: (headers, { getState }) => {
-    const storageData = localStorage.getItem(reducerKey.auth);
-    if (!storageData) {
-      return headers;
-    }
-
-    const token =
-      (getState() as RootState).auth.tokens?.access || JSON.parse(storageData).tokens.access;
-    if (token) {
-      headers.set('Authorization', `Bearer ${token}`);
+    const tokens = (getState() as RootState).auth.tokens;
+    if (tokens) {
+      headers.set('Authorization', `Bearer ${tokens.access}`);
     }
 
     return headers;
   },
 });
 
+export const baseQueryWithReauth: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+  const result = await baseQuery(args, api, extraOptions);
+
+  if (result.error && result.error.status === 401) {
+    const authState = (api.getState() as RootState).auth;
+    const tokens = authState.tokens;
+    if (!tokens) {
+      return result;
+    }
+
+    /**
+     * Because import anything from auth.api.ts cause cycle import
+     */
+    const response = await baseQuery(
+      {
+        url: PATHS.auth.refresh,
+        method: HTTP.post,
+        body: { refreshToken: tokens.refresh },
+      },
+      api,
+      extraOptions,
+    );
+
+    if (response.data) {
+      /**
+       * Because import anything from auth.slice.ts cause cycle import
+       */
+      api.dispatch({ type: 'auth/setState', payload: response.data });
+
+      return baseQuery(args, api, extraOptions);
+    } else {
+      /**
+       * Because import anything from auth.slice.ts cause cycle import
+       */
+      api.dispatch({ type: 'auth/logout' });
+    }
+  }
+
+  return result;
+};
+
 export const api = createApi({
   reducerPath: 'api',
-  baseQuery,
+  baseQuery: baseQueryWithReauth,
   endpoints: () => ({}),
 });
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { useDispatch, useSelector } from 'react-redux';
 import type { TypedUseSelectorHook } from 'react-redux';
-import { config } from '@/config';
 import { api } from './api';
 import { authReducer } from './auth';
+import { boardsReducer } from './boards';
 import { config } from '@/config';
 
 export const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
     auth: authReducer,
+    boards: boardsReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
   devTools: config.isDev,
