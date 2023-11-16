89fe8941f2f39602117b97952ee34ac1dba06f57
leendrew <leendrew99@gmail.com>
2023-11-16 23:14:19 +0600

    chore: refactor WithAuth

--- src/hocs/WithAuth.tsx
+++ src/hocs/WithAuth.tsx
@@ -1,19 +1,14 @@
-import React from 'react';
-import { Navigate, useLocation } from 'react-router-dom';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import { useSelector } from '@/store';
 import { PATHS } from '@/config';
 
-interface WithAuthProps {
-  children: React.ReactElement;
-}
-
-export function WithAuth({ children }: WithAuthProps) {
+export function WithAuth() {
   const location = useLocation();
-  const user = useSelector((store) => store.auth.user);
+  const isAuth = useSelector((store) => !!store.auth.user);
 
-  if (!user) {
+  if (!isAuth) {
     return <Navigate to={PATHS.auth.login} replace state={{ from: location }} />;
   }
 
-  return children;
+  return <Outlet />;
 }
