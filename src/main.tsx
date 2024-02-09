f9a7ba3d11f13b9457886508dbb60db12a08d22d
leendrew <leendrew99@gmail.com>
2024-02-09 20:36:51 +0600

    feat: error toast

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { ThemeProvider, CssBaseline } from '@mui/material';
+import { ToastContainer } from 'react-toastify';
 import { RouterProvider } from 'react-router-dom';
 import { store } from '@/store';
 import { authActions } from '@/store/auth';
 import { theme } from '@/config';
 import { router } from '@/router';
+import 'react-toastify/dist/ReactToastify.min.css';
 import './index.css';
 
 function bootstrap() {
@@ -18,6 +20,7 @@ function bootstrap() {
         <ThemeProvider theme={theme}>
           <RouterProvider router={router} />
           <CssBaseline />
+          <ToastContainer position="bottom-right" theme={muiConfig.palette.mode} draggable />
         </ThemeProvider>
       </Provider>
     </React.StrictMode>,
--- src/store/api.ts
+++ src/store/api.ts
@@ -1,5 +1,6 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 import type { BaseQueryFn, FetchArgs, FetchBaseQueryError } from '@reduxjs/toolkit/query/react';
+import { toast } from 'react-toastify';
 import type { RootState } from './store';
 import { authActions } from './auth/auth.slice';
 import type { AuthResponse } from './auth/auth.types';
@@ -18,40 +19,47 @@ const baseQuery = fetchBaseQuery({
   },
 });
 
-export const baseQueryWithReauth: BaseQueryFn<
+export const baseQueryWithReAuth: BaseQueryFn<
   string | FetchArgs,
   unknown,
   FetchBaseQueryError
 > = async (args, api, extraOptions) => {
   const result = await baseQuery(args, api, extraOptions);
 
-  if (result.error && result.error.status === HTTP_STATUS.unauthorized) {
-    const authState = (api.getState() as RootState).auth;
-    const tokens = authState.tokens;
-    if (!tokens) {
-      return result;
+  if (result.error) {
+    if (result.error.status === HTTP_STATUS.unauthorized) {
+      const authState = (api.getState() as RootState).auth;
+      const tokens = authState.tokens;
+      if (!tokens) {
+        return result;
+      }
+
+      /**
+       * Because import anything from ./auth/auth.api.ts cause cycle import
+       */
+      const response = await baseQuery(
+        {
+          url: PATHS.auth.refresh,
+          method: HTTP.post,
+          body: { refreshToken: tokens.refresh },
+        },
+        api,
+        extraOptions,
+      );
+
+      if (response.data) {
+        api.dispatch(authActions.setState(response.data as AuthResponse));
+
+        return baseQuery(args, api, extraOptions);
+      }
+
+      api.dispatch(authActions.logout());
+    } else {
+      const message =
+        (result.error?.data as { message?: string })?.message || 'Something went wrong';
+      toast.error(message);
+      console.log('@error api', result.error.data, message);
     }
-
-    /**
-     * Because import anything from ./auth/auth.api.ts cause cycle import
-     */
-    const response = await baseQuery(
-      {
-        url: PATHS.auth.refresh,
-        method: HTTP.post,
-        body: { refreshToken: tokens.refresh },
-      },
-      api,
-      extraOptions,
-    );
-
-    if (response.data) {
-      api.dispatch(authActions.setState(response.data as AuthResponse));
-
-      return baseQuery(args, api, extraOptions);
-    }
-
-    api.dispatch(authActions.logout());
   }
 
   return result;
@@ -59,6 +67,6 @@ export const baseQueryWithReauth: BaseQueryFn<
 
 export const api = createApi({
   reducerPath: 'api',
-  baseQuery: baseQueryWithReauth,
+  baseQuery: baseQueryWithReAuth,
   endpoints: () => ({}),
 });
